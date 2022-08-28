832d232adccf832fdc028ae82feac747ca1ab458
Mayar-ElMallah <54380002+Mayar-ElMallah@users.noreply.github.com>
2022-08-28 16:20:47 +0200

    Update server.ts

--- source/server.ts
+++ source/server.ts
@@ -1,46 +1,46 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { filter_Image_From_URL, delete_Local_Files } from './util/util';
 import { reduce } from 'bluebird';
 
 (async () => {
 
-  // Init the Express application
-  const app = express();
+  /*********Init the Express application******/
+  const application = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
 
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+  // Use the bodyParserMiddleware for post requests
+  application.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
+  // IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredImage?image_URL={{URL}}
+  // endpoint to filterImage from a public url.
+  // IT SHOULD:
+  //    1. validate the image_URL query
+  //    2. call filter_Image_From_URL(image_url) in order to filter the image
   //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //    4. deletes any files on the server when the respone is finished
+  /***************QUERY PARAMATERS***************/
+  // Image_url: URL with accessible=public
+  /******************RETURNS*********************/
+  //the_filtered_image_file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
 
-  app.get("/filteredImage", async (req, res) => {
+  application.get("/filteredImage", async (req, res) => {
 
-    const imageUrl = req.query.image_url;
-    if (imageUrl) {
-      filterImageFromURL(imageUrl).then((response) => {
+    const image_Url = req.query.image_url;
+    if (image_Url) {
+      filter_Image_From_URL(image_Url).then((response) => {
         res.sendFile(response);
-        res.on("finish", function () {
-          deleteLocalFiles([response])
+        res.on("finished", function () {
+          delete_Local_Files([response])
         })
       })
     } else {
-      res.status(404).send("The Image url is incorrect, Please provide a valid url");
+      res.status(404).send("The Image url is not found, Please provide a valid url");
     }
   })
 
@@ -50,14 +50,14 @@ import { reduce } from 'bluebird';
   // Root Endpoint
 
 
-  // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  // Displays a clear message to the user
+  application.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
   });
 
 
-  // Start the Server
-  app.listen(port, () => {
+  // Start Server
+  application.listen(port, () => {
     console.log(`server running http://localhost:${port}`);
     console.log(`press CTRL+C to stop server`);
   });
