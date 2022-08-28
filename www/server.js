3b94e00a27c942e1d3c211af9976f1048d21b981
Mayar-ElMallah <54380002+Mayar-ElMallah@users.noreply.github.com>
2022-08-28 16:46:10 +0200

    Update server.js

--- www/server.js
+++ www/server.js
@@ -7,20 +7,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
+var __import_Default = (this && this.__import_Default) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const util_1 = require("./util/util");
+const express1 = __import_Default(require("express"));
+const bodyParser_1 = __import_Default(require("body-parser"));
+const util1 = require("./util/util");
 (() => __awaiter(this, void 0, void 0, function* () {
     // Init the Express application
-    const app = express_1.default();
+    const application = express1.default();
     // Set the network port
     const port = process.env.PORT || 8082;
     // Use the body parser middleware for post requests
-    app.use(body_parser_1.default.json());
+    application.use(bodyparser_1.default.json());
     // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
     // GET /filteredimage?image_url={{URL}}
     // endpoint to filter an image from a public url.
@@ -34,28 +34,28 @@ const util_1 = require("./util/util");
     // RETURNS
     //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
     /**************************************************************************** */
-    app.get("/filteredImage", (req, res) => __awaiter(this, void 0, void 0, function* () {
-        const imageUrl = req.query.image_url;
-        if (imageUrl) {
-            util_1.filterImageFromURL(imageUrl).then((response) => {
+    application.get("/filteredImage", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        const image_Url = req.query.image_url;
+        if (image_Url) {
+            util1.filter_Image_From_URL(image_Url).then((response) => {
                 res.sendFile(response);
-                res.on("finish", function () {
-                    util_1.deleteLocalFiles([response]);
+                res.on("finished", function () {
+                    util1.delete_Local_Files([response]);
                 });
             });
         }
         else {
-            res.status(404).send("The Image url is incorrect, Please provide a valid url");
+            res.status(404).send("The Image url is not found, Please provide a valid url");
         }
     }));
     //! END @TODO1
     // Root Endpoint
     // Displays a simple message to the user
-    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    application.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
         res.send("try GET /filteredimage?image_url={{}}");
     }));
     // Start the Server
-    app.listen(port, () => {
+    application.listen(port, () => {
         console.log(`server running http://localhost:${port}`);
         console.log(`press CTRL+C to stop server`);
     });
